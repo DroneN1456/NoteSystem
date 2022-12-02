5b0a07f71e832454b8019743c190d11e9002b867
DroneN1456 <gugagrilo07@gmail.com>
2022-12-02 13:01:29 -0300

    Pagina modelo pronta

--- frontend/src/MainComponent.vue
+++ frontend/src/MainComponent.vue
@@ -1,6 +1,56 @@
 <template>
-    <div>
-        work in progress
+    <div class="container h-100">
+        <div class="row d-flex h-100 justify-content-center align-items-center">
+            <div class="col col-12">
+                <div class="card p-1">
+                    <div class="row d-flex align-items-center justify-content-center">
+                        <div class="row">
+                            <div class="card boxShadow">
+                                <div class="row d-flex justify-content-start">
+                                    <div class="col col-12">
+                                        <div class="card-title upperCard">23/30</div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col col-12">
+                                        <div class="card-title upperCard">Nome: Jonas</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="card boxShadow">
+                                <div class="row d-flex justify-content-start align-items-center">
+                                    <div class="col col-3">
+                                        <div class="upperCard">Nota: </div>
+                                    </div>
+                                    <div class="col col-9">
+                                        <input type="number" id="notaInput" placeholder="0.0" class="form-control form-control-lg" />
+                                    </div>
+                                </div>
+                                <div class="row d-flex justify-content-start align-items-center">
+                                    <div class="col col-3">
+                                        <div class="upperCard">Motivo: </div>
+                                    </div>
+                                    <div class="col col-9">
+                                        <input type="text" id="motivoInput" placeholder="Porque ele..." class="form-control form-control-lg" />
+                                    </div>
+                                    
+                                </div>
+                                <div class="row d-flex justify-content-start align-items-center">
+                                    <div class="col col-12">
+                                        <button class="btn btn-success bi-check w-100 m-2 boxShadow" @click="nextPerson"></button>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
     </div>
 </template>
 <script>
@@ -9,13 +59,28 @@
 
 
 export default {
-    methods:{
-        toLogin(){
+    methods: {
+        toLogin() {
             this.$router.push("/login")
+        },
+        nextPerson(){
+            //call api setting current person
+
+            //change to the next person in list
+            
         }
-    }, 
+    },
 }
 </script>
-<style lang="">
-    
+<style>
+.upperCard {
+    font-size: 1rem;
+    font-family: 'montserrat';
+    font-weight: bolder;
+}
+
+.boxShadow {
+    box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px inset;
+}
+
 </style>
